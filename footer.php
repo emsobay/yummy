0dbee14d67824c20578da5fab7c21152bfec36c7
walidben8 <161305663+walidben8@users.noreply.github.com>
2024-10-20 20:34:51 +0200

    Update footer.php
    nu de goede footer erin

--- footer.php
+++ footer.php
@@ -1,13 +1,4 @@
-<header>
-        <img src="../img/" alt="">
-        <h1>Chillies Galore</h1>
-        <nav>
-            <ul>
-                <li><a href="index.html">Home</a></li>
-                <li><a href="menu.html">Menu</a></li>
-                <li><a href="gallery.html">Gallery</a></li>
-                <li><a href="about.html">About Us</a></li>
-                <li><a href="reservation.php">Reserveren</a></li>
-            </ul>
-        </nav>
-    </header>
+<!-- footer.php -->
+<footer class="footer-index">
+    <p>&copy; 2024 Chillies Galore</p>
+</footer>
