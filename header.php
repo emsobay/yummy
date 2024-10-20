72fecc014a319f1af22a9b4240101e77f51b02fd
walidben8 <6043897@mborijnland.nl>
2024-10-20 20:33:21 +0200

    header php
    een header php bestand voor elke pagina

---
+++ header.php
@@ -0,0 +1,14 @@
+<!-- header.php -->
+<header>
+    <img src="../img/" alt="">
+    <h1>Chillies Galore</h1>
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="menu.php">Menu</a></li>
+            <li><a href="gallery.php">Gallery</a></li>
+            <li><a href="about.php">About Us</a></li>
+            <li><a href="reservation.php">Reserveren</a></li>
+        </ul>
+    </nav>
+</header>
