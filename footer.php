23b52b1f5b4a236d0aecfdf1b9e7030e8b9e2559
walidben8 <6043897@mborijnland.nl>
2024-10-20 20:31:42 +0200

    footer
    een footer php bestand voor elke pagina

---
+++ footer.php
@@ -0,0 +1,13 @@
+<header>
+        <img src="../img/" alt="">
+        <h1>Chillies Galore</h1>
+        <nav>
+            <ul>
+                <li><a href="index.html">Home</a></li>
+                <li><a href="menu.html">Menu</a></li>
+                <li><a href="gallery.html">Gallery</a></li>
+                <li><a href="about.html">About Us</a></li>
+                <li><a href="reservation.php">Reserveren</a></li>
+            </ul>
+        </nav>
+    </header>
