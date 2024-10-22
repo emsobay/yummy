732dbe4dd2d8b010c86a51904a5acd66ab5f8d54
berko <berkonikky@gmail.com>
2024-10-22 13:56:44 +0200

    added dranken

--- dranken.php
+++ dranken.php
@@ -1,29 +1,38 @@
-<!-- index.php (or any other page) -->
 <!DOCTYPE html>
 <html lang="nl">
 <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible"  content="IE=edge"> 
-        <meta name="description" content="Chilies Galore, dranken.html">
-        <meta name="keywords" content="HTML, Chilies Galore">
-        <meta name="author" content="Berko,Walid,Emin">
-        <title>Chillies Galore | Dranken</title>
-        <link rel="stylesheet" href="css/test.css">
-    </head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
+    <meta name="description" content="Chilies Galore, dranken.html">
+    <meta name="keywords" content="HTML, Chilies Galore">
+    <meta name="author" content="Berko,Walid,Emin"> 
+    <title>Chillies Galore | Dranken</title>
+    <link rel="stylesheet" href="css/test.css">
+</head>
 <body>
 
 <?php include 'header.php'; ?>
 
-<main class="flex-container">
+<main class="menu-container">
     <h2 class="title">Onze Dranken</h2>
-    <div class="drank-item">Drank 1: Omschrijving</div>
-    <div class="drank-item">Drank 2: Omschrijving</div>
-    <div class="drank-item">Drank 3: Omschrijving</div>
+    <div class="drank-item">
+        <img src="img/bellini.jpg" alt="Bellini">
+        <p class="drink-name">Bellini</p>
+        <p class="price">$10</p>
+        <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </div>
+    <div class="drank-item">
+        <img src="img/bloody-mary.jpg" alt="Bloody Mary">
+        <p class="drink-name">Bloody Mary</p>
+        <p class="price">$15</p>
+        <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </div>
+    <!-- Add more drinks here following the same structure -->
 </main>
 
 <?php include 'footer.php'; ?>
 
-
 </body>
 </html>
+
