658b9b091e73d32747fe577269d0f459a6f3b420
emsobay <6044589@mborijnland.nl>
2024-10-23 12:36:52 +0200

    commit arragementen php berwerkt

--- arrangementen.php
+++ arrangementen.php
@@ -15,12 +15,148 @@
 
 <?php include 'header.php'; ?>
 
-<main class="grid-container">
-    <h2 class="title">Arrangementen</h2>
-    <div class="arrangement-item">Arrangement 1: Omschrijving</div>
-    <div class="arrangement-item">Arrangement 2: Omschrijving</div>
-    <div class="arrangement-item">Arrangement 3: Omschrijving</div>
-</main>
+<h1>Onze Arrangementen</h1>
+
+
+<main>
+        <section class="arrangementen-container">
+            <?php
+           
+            $arrangementen = [
+                [
+                    "titel" => "Shared Dining",
+                    "beschrijving" => "Geniet van een gezamenlijke eetervaring met een verscheidenheid aan gerechten en dranken.",
+                    "prijs" => "€29,99",
+                    "gerechten" => [
+                        [
+                            "naam" => "Carne Asada Tacos",
+                            "beschrijving" => "Gegrild gemarineerd rundvlees, koriander, uien en limoen. Geserveerd met salsa.",
+                            "prijs" => "$10.99"
+                        ],
+                        [
+                            "naam" => "Al Pastor Tacos",
+                            "beschrijving" => "Kruidige gemarineerde varkensvlees met ananas, koriander en uien.",
+                            "prijs" => "$9.99"
+                        ],
+                        [
+                            "naam" => "Fish Tacos",
+                            "beschrijving" => "Knapperige gefrituurde vis, kool slaw en chipotle mayo op een maïstortilla.",
+                            "prijs" => "$11.99"
+                        ]
+                    ],
+                    "dranken" => [
+                        "Belini",
+                        "Bloody Mary",
+                        "Black Russian",
+                        "Cosmopolitan",
+                        "Caipirinha",
+                        "Champagne Cocktail",
+                        "Cuba Libre",
+                        "Godmother",
+                        "Godfather",
+                        "Cola",
+                        "Fanta",
+                        "Sprite",
+                        "Water"
+                    ]
+                ],
+                [
+                    "titel" => "3 Gangenmenu inclusief drankjes",
+                    "beschrijving" => "Verwen jezelf met een heerlijk driegangenmenu met bijpassende drankjes.",
+                    "prijs" => "€39,99",
+                    "gerechten" => [
+                        [
+                            "naam" => "Chicken Burrito",
+                            "beschrijving" => "Gegrilde kip, rijst, bonen, kaas, zure room en salsa gewikkeld in een tortillawrap.",
+                            "prijs" => "$8.99"
+                        ],
+                        [
+                            "naam" => "Vegetarian Burrito",
+                            "beschrijving" => "Rijst, bonen, kaas, gegrilde groenten en guacamole in een tortillawrap.",
+                            "prijs" => "$7.99"
+                        ],
+                        [
+                            "naam" => "Barbacoa Burrito",
+                            "beschrijving" => "Langzaam gekookt rundvlees, rijst, bonen, kaas en pico de gallo gewikkeld in een tortillawrap.",
+                            "prijs" => "$9.99"
+                        ]
+                    ],
+                    "dranken" => [
+                        "Cola",
+                        "Fanta",
+                        "Sprite",
+                        "Water",
+                        "Belini",
+                        "Bloody Mary",
+                        "Black Russian",
+                        "Cosmopolitan",
+                        "Caipirinha",
+                        "Champagne Cocktail",
+                        "Cuba Libre",
+                        "Godmother",
+                        "Godfather"
+                    ]
+                ],
+                [
+                    "titel" => "Arrangement voor Groepen/Families",
+                    "beschrijving" => "Ideaal voor grote gezelschappen, met speciale menu-opties.",
+                    "prijs" => "€99,99",
+                    "gerechten" => [
+                        [
+                            "naam" => "Cheese Quesadilla",
+                            "beschrijving" => "Classic quesadilla met gesmolten kaas in een gegrilde tortillawrap.",
+                            "prijs" => "$6.99"
+                        ],
+                        [
+                            "naam" => "Chicken Fajitas",
+                            "beschrijving" => "Sissende gegrilde kip met paprika's en uien. Geserveerd met tortilla's, guacamole en zure room.",
+                            "prijs" => "$14.99"
+                        ],
+                        [
+                            "naam" => "Vegetarian Fajitas",
+                            "beschrijving" => "Gegrilde groenten geserveerd met tortilla's, guacamole en salsa.",
+                            "prijs" => "$12.99"
+                        ]
+                    ],
+                    "dranken" => [
+                        "Belini",
+                        "Cosmopolitan",
+                        "Cuba Libre",
+                        "Godmother",
+                        "Godfather",
+                        "Water"
+                        "Cola",
+                        "Fanta",
+                        "Sprite"
+                    ]
+                ]
+            ];
+
+            // Loop door de arrangementen en genereer HTML
+            foreach ($arrangementen as $arrangement) {
+                echo "<section class='arrangement'>";
+                echo "<h2>" . $arrangement['titel'] . "</h2>";
+                echo "<p>" . $arrangement['beschrijving'] . "</p>";
+                echo "<p><strong>Prijs: " . $arrangement['prijs'] . "</strong></p>";
+                
+                echo "<h3>Gerechten:</h3><ul>";
+                foreach ($arrangement['gerechten'] as $gerecht) {
+                    echo "<li><strong>" . $gerecht['naam'] . ":</strong> " . $gerecht['beschrijving'] . " <em>(" . $gerecht['prijs'] . ")</em></li>";
+                }
+                echo "</ul>";
+
+                echo "<h3>Dranken:</h3><ul>";
+                foreach ($arrangement['dranken'] as $drank) {
+                    echo "<li>" . $drank . "</li>";
+                }
+                echo "</ul>";
+
+                echo "</section>";
+            }
+            ?>
+        </section>
+    </main>
+
 
 <?php include 'footer.php'; ?>
 
