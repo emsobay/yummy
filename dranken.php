fb741f5663df0cd4673dfc16c0c11a60c750e862
berko <berkonikky@gmail.com>
2024-10-23 12:54:49 +0200

    section fix

--- dranken.php
+++ dranken.php
@@ -18,110 +18,110 @@
 <main class="menu-container">
     <h1 class="title">Onze Dranken</h1>
     
-    <div class="drank-item">
+    <section class="drank-item">
         <img src="../yummy/img/bellini_cocktail.jpg" alt="Bellini">
         <p class="drink-name">Bellini</p>
         <p class="price">$19,99</p>
         <p class="description">Ingrediënten: Prosecco, perzikpuree.</p>
-    </div>
+    </section>
     
-    <div class="drank-item">
+    <section class="drank-item">
         <img src="../yummy/img/bloodymary_dranken.webp" alt="Bloody Mary">
         <p class="drink-name">Bloody Mary</p>
         <p class="price">$19,99</p>
         <p class="description">Ingrediënten: Wodka, tomatensap, citroensap, Worcestersaus, tabasco, zout, peper.</p>
-    </div>
+    </section>
     
-    <div class="drank-item">
+    <section class="drank-item">
         <img src="../yummy/img/black_russoan_cocktail.png" alt="Black Russian">
         <p class="drink-name">Black Russian</p>
         <p class="price">$19,99</p>
         <p class="description">Ingrediënten: Wodka, koffielikeur.</p>
-    </div>
+    </section>
     
-    <div class="drank-item">
+    <section class="drank-item">
         <img src="../yummy/img/cosmopolitan_cocktail.jpg" alt="Cosmopolitan">
         <p class="drink-name">Cosmopolitan</p>
         <p class="price">$19,99</p>
         <p class="description">Ingrediënten: Wodka, cranberrysap, triple sec, limoensap.</p>
-    </div>
+    </section>
     
-    <div class="drank-item">
+    <section class="drank-item">
         <img src="../yummy/img/Caipirinha_cocktails.jpg" alt="Caipirinha">
         <p class="drink-name">Caipirinha</p>
         <p class="price">$19,99</p>
         <p class="description">Ingrediënten: Cachaça, suiker, limoensap.</p>
-    </div>
+    </section>
     
-    <div class="drank-item">
+    <section class="drank-item">
         <img src="../yummy/img/champagne_cocktail.webp" alt="Champagne Cocktail">
         <p class="drink-name">Champagne Cocktail</p>
         <p class="price">$19,99</p>
         <p class="description">Ingrediënten: Champagne, suikerklontje, Angostura bitters.</p>
-    </div>
+    </section>
     
-    <div class="drank-item">
+    <section class="drank-item">
         <img src="../yummy/img/cuba_libre_cocktail.jpg" alt="Cuba Libre">
         <p class="drink-name">Cuba Libre</p>
         <p class="price">$19,99</p>
         <p class="description">Ingrediënten: Witte rum, cola, limoensap.</p>
-    </div>
+    </section>
     
-    <div class="drank-item">
+    <section class="drank-item">
         <img src="../yummy/img/god_mother_cocktail.jpg" alt="Godmother">
         <p class="drink-name">Godmother</p>
         <p class="price">$19,99</p>
         <p class="description">Ingrediënten: Wodka, amaretto.</p>
-    </div>
+    </section>
     
-    <div class="drank-item">
+    <section class="drank-item">
         <img src="../yummy/img/margarite_cocktail.jpg" alt="Margharita">
         <p class="drink-name">Margharita</p>
         <p class="price">$19,99</p>
         <p class="description">Ingrediënten: Tequila, triple sec, limoensap.</p>
-    </div>
+    </section>
     
-    <div class="drank-item">
+    <section class="drank-item">
         <img src="../yummy/img/rose_cocktail.webp" alt="Rose">
         <p class="drink-name">Rose</p>
         <p class="price">$19,99</p>
         <p class="description">Ingrediënten: Droge witte wijn, kirsch, grenadine.</p>
-    </div>
+    </section>
     
-    <div class="drank-item">
+    <section class="drank-item">
         <img src="../yummy/img/sazerac_cocktail.webp" alt="Sazerac">
         <p class="drink-name">Sazerac</p>
         <p class="price">$19,99</p>
         <p class="description">Ingrediënten: Cognac of roggewhiskey, absint, Peychaud's bitters, suiker.</p>
-    </div>
+    </section>
     
-    <div class="drank-item">
+    <section class="drank-item">
         <img src="../yummy/img/sea_breeze_cocktail.jpg" alt="Sea Breeze">
         <p class="drink-name">Sea Breeze</p>
         <p class="price">$19,99</p>
         <p class="description">Ingrediënten: Wodka, cranberrysap, grapefruitsap.</p>
-    </div>
+    </section>
     
-    <div class="drank-item">
+    <section class="drank-item">
         <img src="../yummy/img/sea_on_the_beach.jpg" alt="Sex on the Beach">
         <p class="drink-name">Sex on the Beach</p>
         <p class="price">$19,99</p>
         <p class="description">Ingrediënten: Wodka, perziklikeur, cranberrysap, sinaasappelsap.</p>
-    </div>
+    </section>
     
-    <div class="drank-item">
+    <section class="drank-item">
         <img src="../yummy/img/yellow_bird_cocktail.jpg" alt="Yellow Bird">
         <p class="drink-name">Yellow Bird</p>
         <p class="price">$19,99</p>
         <p class="description">Ingrediënten: Witte rum, Galliano, triple sec, limoensap.</p>
-    </div>
+    </section>
     
-    <div class="drank-item">
+    <section class="drank-item">
         <img src="../yummy/img/vesper_cocktail.png" alt="Vesper">
         <p class="drink-name">Vesper</p>
         <p class="price">$19,99</p>
         <p class="description">Ingrediënten: Gin, wodka, Kina Lillet.</p>
-    </div>
+    </section>
 </main>
 
 <?php include 'footer.php'; ?>
